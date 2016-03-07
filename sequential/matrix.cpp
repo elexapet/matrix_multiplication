4df63172c521e7b7c8f6840f79696c474e7326bd
Petr <remaerc@gmail.com>
2016-03-07 01:32:34 +0100

    strassen exc. padding

--- sequential/matrix.cpp
+++ sequential/matrix.cpp
@@ -2,15 +2,16 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <cstdint>
 
 using namespace std;
 
 //-----------------------------------------------------------------------//
 //-----------------------------------------------------------------------//
 
-void freeMatrix ( double ** array, int rows )
+void freeMatrix ( double ** array, uint64_t rows )
 {
-   for ( int i=0; i < rows; i++ ) delete [] array[i];
+   for ( uint64_t i=0; i < rows; i++ ) delete [] array[i];
    delete [] array;
 }
 
@@ -21,10 +22,10 @@ class CSizeException
 {
 private:
 	const char op;
-	const int r1, c1, r2, c2;
+	const uint64_t r1, c1, r2, c2;
 public:
-	CSizeException	( const int & a1, const int & a2, const int & b1,
-					  const int & b2, const char zn) : op (zn), r1(a1), c1(a2), r2(b1), c2(b2) {}
+	CSizeException	( const uint64_t & a1, const uint64_t & a2, const uint64_t & b1,
+					  const uint64_t & b2, const char zn) : op (zn), r1(a1), c1(a2), r2(b1), c2(b2) {}
 	friend ostream & operator << ( ostream & os, const CSizeException & in );
 };
 
@@ -43,9 +44,9 @@ ostream & operator << ( ostream & os, const CSizeException & in )
 class CIndexException
 {
 private:
-	const int r, c;
+	const uint64_t r, c;
 public:
-	CIndexException ( const int & a1, const int & a2 ) : r(a1), c(a2) {}
+	CIndexException ( const uint64_t & a1, const uint64_t & a2 ) : r(a1), c(a2) {}
 	friend ostream & operator << ( ostream & os, const CIndexException & in );
 };
 
@@ -62,64 +63,87 @@ ostream & operator << ( ostream & os, const CIndexException & in )
 //-----------------------------------------------------------------------//
 //-----------------------------------------------------------------------//
 
+class CSubviewAssigmentException
+{
+private:
+public:
+	CSubviewAssigmentException ( ) {}
+	friend ostream & operator << ( ostream & os, const CSubviewAssigmentException & in );
+};
+
+ostream & operator << ( ostream & os, const CSubviewAssigmentException & in )
+{
+	os << "Assigning matrice to subview of different size is not allowed";
+	return os;
+}
+
+//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
+
 class CMatrix
 {
 private:
-	uint64_t rows;
-	uint64_t rmax;
-	uint64_t cols;
-	double ** m_Matrix;
-	bool isSubMatrix
+	uint64_t offrows; //Offset for submatrices rows beginning
+	uint64_t offcols; //Offset for submatrices columns beginning
+	uint64_t rows; //Allocated rows
+	uint64_t cols; //Allocated columns
+	uint64_t rmax; //Used for matrice parsing
+	double ** m_Matrix; //2D array
+	bool isSubMatrice; //Submatrice mark
 public:
-	CMatrix ( int r = 0, int c = 0, bool ctrl = 0 )
+	//Constructor
+	CMatrix ( uint64_t r = 0, uint64_t c = 0, bool ctrl = 0 )
 	{	
-		isSubMatrix = false;
+		isSubMatrice = false;
+		offrows = offcols = 0;
 
 		cols = c;
 		m_Matrix = new double* [rows = r];
 
-		for (int i = 0; i < rows; ++i)
+		for (uint64_t i = 0; i < rows; ++i)
 			m_Matrix[i] = new double [cols];	
 		if (!ctrl)
 		{
-			for (int i = 0; i < rows; ++i)
-				for (int j = 0; j < cols; ++j)
+			for (uint64_t i = 0; i < rows; ++i)
+				for (uint64_t j = 0; j < cols; ++j)
 				{
-					m_Matrix[i][j] = 0;
+					m_Matrix[i][j] = .0;
 				}
 		}
 	}
-
+	//Copy constructor
 	CMatrix ( const CMatrix & right )
 	{
-		isSubMatrix = false;
+		isSubMatrice = false;
+		offcols = offrows = 0;
 
 		cols = right.cols;
 		rows = right.rows;
 		m_Matrix = new double* [rows];
 
-		for (int i = 0; i < rows; ++i)
+		for (uint64_t i = 0; i < rows; ++i)
 			m_Matrix[i] = new double [cols];
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j){
-					m_Matrix[i][j] = right.m_Matrix[i][j];
-				}
+		for (uint64_t i = 0; i < rows; ++i)
+			for (uint64_t j = 0; j < cols; ++j){
+				m_Matrix[i][j] = right.m_Matrix[i][j];
+			}
 	}
 
-	//constructs submatrix of right without copying its content
-	CMatrix (const CMatrix & right, uint64_t rows, uint64_t cols)
+	//Constructs submatrices of right without copying its content (subview)
+	CMatrix (const CMatrix & right, uint64_t fromRows, uint64_t fromCols, uint64_t rows, uint64_t cols)
 	{
-		isSubMatrix = true;
-
-		this.cols = cols;
-		this.rows = rows;
-		m_Matrix = right.m_Matrix
+		isSubMatrice = true;
+		offrows = fromRows;
+		offcols = fromCols;
+		this->cols = cols;
+		this->rows = rows;
+		m_Matrix = right.m_Matrix;
 	}
 
 	~CMatrix ()
 	{
-		if (isSubMatrix == false){
-			for (int i = 0; i < rows; ++i){
+		if (isSubMatrice == false){
+			for (uint64_t i = 0; i < rows; ++i){
 				delete [] m_Matrix[i];
 			}
 			delete [] m_Matrix;
@@ -128,119 +152,123 @@ public:
 
 	CMatrix & operator = ( const CMatrix & right )
 	{
-		if ( this == &right ) return *this;
-		freeMatrix( m_Matrix, rows );
-		if (cols != right.cols || rows != right.rows){
+		if (this == &right) return *this;
+
+		if (getWidth() != right.getWidth() || getHeight() != right.getHeight()){
+			if (isSubMatrice) throw CSubviewAssigmentException();
+			freeMatrix( m_Matrix, rows );
 			cols = right.cols;
 			rows = right.rows;
 			m_Matrix = new double* [rows];
 		
-			for (int i = 0; i < rows; ++i)
+			for (uint64_t i = 0; i < rows; ++i)
 				m_Matrix[i] = new double [cols];
 		}
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j)
+		for (uint64_t i = 0; i < getHeight(); ++i)
+			for (uint64_t j = 0; j < getWidth(); ++j)
 				{
-					m_Matrix[i][j] = right.m_Matrix[i][j];
+					m_Matrix[i+offrows][j+offcols] = right.m_Matrix[i+right.offrows][j+right.offcols];
 				}
 		return *this;
 	}
 
 	CMatrix operator + ( const CMatrix & right ) const
 	{
-		if ( rows != right.rows || cols != right.cols )
-			throw CSizeException ( rows, cols, right.rows, right.cols, '+' );
-		CMatrix tmp ( rows, cols, true );
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j)
+		if (getWidth() != right.getWidth() || getHeight() != right.getHeight())
+			throw CSizeException ( getHeight(), getWidth(), right.getHeight(), right.getWidth(), '+' );
+		CMatrix tmp ( getHeight(), getWidth(), true );
+		for (uint64_t i = 0; i < tmp.rows; ++i)
+			for (uint64_t j = 0; j < tmp.cols; ++j)
 			{
-				tmp.m_Matrix[i][j] = m_Matrix[i][j] + right.m_Matrix[i][j]; 
+				tmp.m_Matrix[i][j] = m_Matrix[i+offrows][j+offcols] + right.m_Matrix[i+right.offrows][j+right.offcols]; 
 			}
 		return tmp;
 	}
 
 	CMatrix operator - ( const CMatrix & right ) const
 	{
-		if ( rows != right.rows || cols != right.cols )
-			throw CSizeException ( rows, cols, right.rows, right.cols, '-' );
-		CMatrix tmp ( rows, cols, true );
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j)
+		if (getWidth() != right.getWidth() || getHeight() != right.getHeight())
+			throw CSizeException ( getHeight(), getWidth(), right.getHeight(), right.getWidth(), '-' );
+		CMatrix tmp ( getHeight(), getWidth(), true );
+		for (uint64_t i = 0; i < tmp.rows; ++i)
+			for (uint64_t j = 0; j < tmp.cols; ++j)
 			{
-				tmp.m_Matrix[i][j] = m_Matrix[i][j] - right.m_Matrix[i][j]; 
+				tmp.m_Matrix[i][j] = m_Matrix[i+offrows][j+offcols] - right.m_Matrix[i+right.offrows][j+right.offcols]; 
 			}
 		return tmp;	
 	}
 
 	CMatrix operator - ( void ) const
 	{
-		CMatrix tmp ( rows, cols, true );
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j)
+		CMatrix tmp ( getHeight(), getWidth(), true );
+		for (uint64_t i = 0; i < tmp.rows; ++i)
+			for (uint64_t j = 0; j < tmp.cols; ++j)
 			{
-				tmp.m_Matrix[i][j] = -1 * m_Matrix[i][j];
+				tmp.m_Matrix[i][j] = -m_Matrix[i+offrows][j+offcols];
 			}
 		return tmp;		
 	}
 
 	CMatrix operator * ( const CMatrix & right ) const
 	{
-		if ( cols != right.rows )
-			throw CSizeException ( rows, cols, right.rows, right.cols, '*' );
-		CMatrix tmp ( rows, right.cols );
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < right.cols; ++j)
-				for (int k = 0; k < cols; ++k)
+		if ( getWidth() != right.getHeight() )
+			throw CSizeException ( getHeight(), getWidth(), right.getHeight(), right.getWidth(), '*' );
+		CMatrix tmp ( getHeight(), right.getWidth() );
+		for (uint64_t i = 0; i < getHeight(); ++i)
+			for (uint64_t j = 0; j < right.getWidth(); ++j)
+				for (uint64_t k = 0; k < getWidth(); ++k)
 				{
-					tmp.m_Matrix[i][j] += m_Matrix[i][k] * right.m_Matrix[k][j];
+					tmp.m_Matrix[i][j] += m_Matrix[i+offrows][k+offcols] * right.m_Matrix[k+right.offrows][j+right.offcols];
 				}
 		return tmp;	
 	}
 
 	CMatrix operator * ( const double num ) const
 	{
-		CMatrix tmp ( rows, cols, true );
-		for (int i = 0; i < rows; ++i)
-			for (int j = 0; j < cols; ++j)
+		CMatrix tmp ( getHeight(), getWidth(), true );
+		for (uint64_t i = 0; i < tmp.rows; ++i)
+			for (uint64_t j = 0; j < tmp.cols; ++j)
 			{
-				tmp.m_Matrix[i][j] = num * m_Matrix[i][j];
+				tmp.m_Matrix[i][j] = num * m_Matrix[i+offrows][j+offcols];
 			}
 		return tmp;
 	}
 
 	class Proxy {
     public:
-        Proxy( double* in, const int r, const int & rmax, const int & cmax ) 
-				: m_Array(in), r(r), cmax(cmax), rmax(rmax) {}
-        double & operator [] ( const int c ) const
+        Proxy( double* in, const uint64_t r, const uint64_t & rmax, const uint64_t & cmax,
+        		 const uint64_t & sr,  const uint64_t & sc) 
+				: m_Array(in), r(r), cmax(cmax), rmax(rmax), sr(sr), sc(sc) {}
+        double & operator [] ( const uint64_t c ) const
         {
-            if ( c < 0 || c >= cmax || r < 0 || r >= rmax ) throw CIndexException ( r, c );
+            if ( c < sc || c >= cmax || r < sr || r >= rmax ) throw CIndexException ( r, c );
 			return m_Array[c];
         }
     private:
         double* m_Array;
-        int r, cmax, rmax;
+        uint64_t r, cmax, rmax, sr, sc;
     };
 
-    Proxy operator [] ( const int r ) const
+    Proxy operator [] ( const uint64_t r ) const
     {
-        return Proxy( m_Matrix[r], r, rows, cols );
+        return Proxy( m_Matrix[r], r, rows, cols, offrows, offcols );
     }
 
-    uint64_t getWidth()
+    uint64_t getWidth() const
     {
-    	return cols;
+    	return cols - offcols;
     }
 
-    uint64_t getHeight()
+    uint64_t getHeight() const
     {
-    	return rows;
+    	return rows - offrows;
     }
 
+    //Our friends
 	friend ostream & operator << ( ostream & os, const CMatrix & in );
 	friend istream & operator >> ( istream & is, CMatrix & out );
 	friend CMatrix strassenMult(const CMatrix & left, const CMatrix & right);
-	friend CMatrix strassenRecursion(const CMatrix & left, const CMatrix & right);
+	friend CMatrix strassenRec(const CMatrix & left, const CMatrix & right);
 
 };
 
@@ -250,14 +278,14 @@ public:
 ostream & operator << ( ostream & os, const CMatrix & in )
 {
 	os << "{";
-	for (int i = 0; i < in.rows; ++i)
+	for (uint64_t i = 0; i < in.rows; ++i)
 	{
-		if ( i > 0 ) os << ", ";
+		if ( i > 0 ) os << ", \n";
 		os << "{";
-		for (int j = 0; j < in.cols; ++j)
+		for (uint64_t j = 0; j < in.cols; ++j)
 		{
 			if ( j > 0 ) os << ", ";
-			os << in.m_Matrix[i][j];
+			os << in.m_Matrix[i+in.offrows][j+in.offcols];
 		}
 		os << "}";
 	}	
@@ -270,7 +298,7 @@ ostream & operator << ( ostream & os, const CMatrix & in )
 
 istream & operator >> ( istream & is, CMatrix & out )
 {
-	int unpaired = 0, r = 0, c, refc = 0, rmax = 64, cmax = 64;
+	uint64_t unpaired = 0, r = 0, c, refc = 0, rmax = 64, cmax = 64;
 	CMatrix tmp ( rmax, cmax );
 	double num;
 	char zn;
@@ -296,8 +324,8 @@ istream & operator >> ( istream & is, CMatrix & out )
 					CMatrix reloc = tmp;
 					tmp.~CMatrix();
 					CMatrix tmp ( rmax, cmax );
-					for (int i = 0; i < r; ++i)
-						for (int j = 0; j < c; ++j)
+					for (uint64_t i = 0; i < r; ++i)
+						for (uint64_t j = 0; j < c; ++j)
 						{
 							tmp[i][j] = reloc[i][j];
 						}
@@ -321,8 +349,8 @@ istream & operator >> ( istream & is, CMatrix & out )
 			CMatrix reloc = tmp;
 			tmp.~CMatrix();
 			CMatrix tmp ( rmax, cmax );
-			for (int i = 0; i < r; ++i)
-				for (int j = 0; j < refc; ++j)
+			for (uint64_t i = 0; i < r; ++i)
+				for (uint64_t j = 0; j < refc; ++j)
 				{
 					tmp[i][j] = reloc[i][j];
 				}	
@@ -337,11 +365,11 @@ istream & operator >> ( istream & is, CMatrix & out )
 	out.cols = refc;
 	out.rows = r;
 	out.m_Matrix = new double* [out.rows];
-	for (int i = 0; i < out.rows; ++i)
+	for (uint64_t i = 0; i < out.rows; ++i)
 		out.m_Matrix[i] = new double [out.cols];
 		
-	for (int i = 0; i < out.rows; ++i)
-		for (int j = 0; j < out.cols; ++j)
+	for (uint64_t i = 0; i < out.rows; ++i)
+		for (uint64_t j = 0; j < out.cols; ++j)
 			out.m_Matrix[i][j] = tmp[i][j];
 	return is;
 }
@@ -351,9 +379,6 @@ istream & operator >> ( istream & is, CMatrix & out )
 
 int main(int argc, char const *argv[])
 {
-	istringstream is;
-	ostringstream os;
-	
 	CMatrix matrixA;
 	CMatrix matrixB;
 	ifstream infileA("matrixA.txt");
@@ -376,7 +401,17 @@ int main(int argc, char const *argv[])
 	cout << "\n\n";
 	cout << matrixB;
 	cout << "\n\n";
-	cout << matrixA*matrixB;
+	cout << "Normal:\n" << matrixA*matrixB;
+	cout << "\n\n";
+	try{
+		cout << "Strassen:\n" << strassenMult(matrixA,matrixB);
+	}catch(CSizeException &e){
+		cout << e << '\n';
+	}catch(CIndexException &e){
+		cout << e << '\n';
+	}catch(CSubviewAssigmentException &e){
+		cout << e << '\n';
+	}
 
 	return 0;
 }
--- sequential/strassen.cpp
+++ sequential/strassen.cpp
@@ -1,45 +1,73 @@
+#include "matrix.cpp"
 
-#define STRASS_SIZE_LIMIT 512
+#define STRASS_SIZE_LIMIT 16
 
 //-----------------------------------------------------------------------//
 //-----------------------------------------------------------------------//
 
 CMatrix strassenMult(const CMatrix & left, const CMatrix & right)
 {
-	//Use standard multiplication when matrixes are small enough.
+	if (left.getWidth() != left.getHeight() || right.getHeight() != right.getWidth()){
+		cout << "Matrices are not square.";
+		throw CSizeException ( left.getHeight(), left.getWidth(), right.getHeight(), right.getWidth(), '*' );
+	}
+	//Use standard multiplication when matrices are small enough.
 	if (left.getWidth() < STRASS_SIZE_LIMIT || right.getWidth() < STRASS_SIZE_LIMIT)
 		return left*right;
 
-	//padd matrixes to 2^N x 2^N size
+	//padd matrices to 2^N x 2^N size
 	//todo
 
-	return strassenRecursion(left, right);
-)
+	return strassenRec(left, right);
+}
 
-CMatrix strassenRecursion(const CMatrix & left, const CMatrix & right){
-	//Use standard multiplication when matrixes are small enough.
+CMatrix strassenRec(const CMatrix & left, const CMatrix & right){
+	//Use standard multiplication when matrices are small enough.
 	if (left.getWidth() < STRASS_SIZE_LIMIT || right.getWidth() < STRASS_SIZE_LIMIT)
 		return left*right;
 
-	CMatrix *m1,*m2,*m3,*m4,*m5,*m6,*m7;
-	CMatrix result(left.rows, right.cols, true);
+	//result placeholder
+	CMatrix result(left.getHeight(), left.getWidth(), true);
 
-	//todo fix generation to produce submatrix not only from 0,0 to x,y
-	m1 = &strassenRecursion((CMatrix(left,1,1) + CMatrix(left,2,2),(CMatrix(right,1,1) + CMatrix(right,2,2)))
-	m2 = &strassenRecursion((CMatrix(left,2,1) + CMatrix(left,2,2), CMatrix(right,1,1))
-	m3 = &strassenRecursion(CMatrix(left,1,1), (CMatrix(right,1,2) - CMatrix(right,2,2)))
-	m4 = &strassenRecursion(CMatrix(left,2,2) , (CMatrix(right,2,1) - CMatrix(right,1,1)))
-	m5 = &strassenRecursion((CMatrix(left,1,1) + CMatrix(left,1,2)), CMatrix(right,2,2))
-	m6 = &strassenRecursion((CMatrix(left,2,1) - CMatrix(left,1,1)), (CMatrix(right,1,1) + CMatrix(right,1,2)))
-	m7 = &strassenRecursion((CMatrix(left,1,2) - CMatrix(left,2,2)), (CMatrix(right,2,1) + CMatrix(right,2,2)))
+	uint64_t halfr = result.getHeight()/2;
+	uint64_t halfc = result.getWidth()/2;
+	uint64_t rows = result.getHeight();
+	uint64_t cols = result.getWidth();
 
-	CMatrix(result, 1, 1) = *m1 + *m4 - *m5 + *m7;
-	CMatrix(result, 1, 2) = *m3 + *m5;
-	CMatrix(result, 2, 1) = *m2 + *m4;
-	CMatrix(result, 2, 2) = *m1 - *m2 + *m3 + *m6;
+	//subviews of left & right
+	CMatrix a11(left,0,0,halfr,halfc);
+	CMatrix a12(left,0,halfc,halfr,cols);
+	CMatrix a21(left,halfr,0,rows,halfc);
+	CMatrix a22(left,halfr,halfc,rows,cols);
+	CMatrix b11(right,0,0,halfr,halfc);
+	CMatrix b12(right,0,halfc,halfr,cols);
+	CMatrix b21(right,halfr,0,rows,halfc);
+	CMatrix b22(right,halfr,halfc,rows,cols);
 
-	return C;
-)
+	//new submatrices m(i)
+	CMatrix const & m1 = strassenRec(a11+a22,b11+b22);
+	//cout << m1 << "\n";
+	CMatrix const & m2 = strassenRec(a21+a22,b11);
+	//cout << m2<< "\n";
+	CMatrix const & m3 = strassenRec(a11,b12-b22);
+	//cout << m3<< "\n";
+	CMatrix const & m4 = strassenRec(a22,b21-b11);
+	//cout << m4<< "\n";
+	CMatrix const & m5 = strassenRec(a11+a12,b22);
+	//cout << m5<< "\n";
+	CMatrix const & m6 = strassenRec(a21-a11,b11+b12);
+	//cout << m6<< "\n";
+	CMatrix const & m7 = strassenRec(a12-a22,b21+b22);
+	//cout << m7<< "\n";
+
+	//express result with m(i) matrices
+	CMatrix(result, 0,0, 		halfr,halfc) = m1 + m4 - m5 + m7;
+	CMatrix(result, 0,halfc, 	halfr,cols) = m3 + m5;
+	CMatrix(result, halfr,0, 	rows,halfc) = m2 + m4;
+	CMatrix(result, halfr,halfc,rows,cols) = m1 - m2 + m3 + m6;
+
+	return result;
+}
 
 //-----------------------------------------------------------------------//
 //-----------------------------------------------------------------------//
